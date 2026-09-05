fecc438fd23fdc1b90084ac87e2fbc47555fb7f4
Gianpaolo P. <68400366+pgpaolo@users.noreply.github.com>
2026-09-05 21:05:20 +0200

    Fix OTA write buffer compatibility

--- src/firmware_update.h
+++ src/firmware_update.h
@@ -8,7 +8,7 @@ void serviceFirmwareUpdate();
 String firmwareUpdateStatusJson();
 
 bool firmwareRemoteBegin(size_t imageSize, const String &sha256Hex, String &error);
-bool firmwareRemoteWrite(uint32_t sequence, const uint8_t *data, size_t len, String &error);
+bool firmwareRemoteWrite(uint32_t sequence, uint8_t *data, size_t len, String &error);
 bool firmwareRemoteEnd(String &error);
 void firmwareRemoteAbort(const String &reason = String());
 bool firmwareUpdateInProgress();
